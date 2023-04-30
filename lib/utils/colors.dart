6225b1df7ff51e54ca22fe0a65722e83a8e7745b
aaron <alkrauss48@gmail.com>
2023-04-30 15:20:15 -0500

    feat: add home bottom tab bar and initial colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color darkYellow = Color(0xFFe1ab0b);
+  static const Color yellow = Color(0xFFf6c944);
+  static const Color lightYellow = Color(0xFFfbe4a2);
+
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color grey = Color(0xFF9e9e9e);
+  static const Color lightGrey = Color(0xFFe0e0e0);
+  static const Color darkGrey = Color(0xFF424242);
+}
